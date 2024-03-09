b33429dfd9efe811e01fee7d852d4fe7c4c40387
Divye Joshi <divyej90_soe@jnu.ac.in>
2024-03-09 13:29:07 +0530

    Update Collider.h

--- Core/Source/Physics/Collider.h
+++ Core/Source/Physics/Collider.h
@@ -2,29 +2,29 @@
 
 #include "PhysicsObject.h"
 #include "Shape.h"
+#include "Vec2.h"
 
 class Collider : public PhysicsObject
 {
 public:
-	Collider(Vec2 theVelocity, Vec2 thePosition,
-            bool theGravityEnabled, bool theCollisionEnabled, Shape theShape);
-	/**
-	 * Check if the collider objects intersect
-	 * @param  Collider theOther
-	 * @return bool
-	 */
-	bool Intersects(const Collider& theOther) const;
-	/**
-	 * Updates the velocity of the collider objects on collision
-	 * @param  Collider theFirst
-	 * @param  Collider theSecond
-	 * @param  f32 theCoefficientOfRestitution
-	 */
-	void ResolveCollision(Collider& theFirst, Collider& theSecond, f32 theCoefficientOfRestitution=1.0f);
+  Collider(Vec2 theVelocity, Vec2 thePosition,
+          bool theGravityEnabled, bool theCollisionEnabled, Shape theShape);
+  /**
+   * Check if the collider objects intersect
+   * @param  Collider theOther
+   * @return bool
+   */
+  bool Intersects(const Collider& theOther) const;
+  /**
+   * Updates the velocity of the collider objects on collision
+   * @param  Collider theFirst
+   * @param  Collider theSecond
+   * @param  f32 theCoefficientOfRestitution
+   */
+  void ResolveCollision(Collider& theFirst, Collider& theSecond, f32 theCoefficientOfRestitution = 1.0f);
 
-    Shape m_Shape;
 private:
+  Shape m_Shape;
 
-	bool intersectCircleCircle(const Collider& theFirst, const Collider& theSecond) const;
+  bool intersectCircleCircle(const Collider& theFirst, const Collider& theSecond) const;
 };
-
